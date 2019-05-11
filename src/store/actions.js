90dc9578e4fea3560261f442be0cfe1da43160f8
Philipp Moritz <pcmoritz@gmail.com>
2019-05-11 22:08:22 +0000

    update

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import { axios } from '../utils'
 
 // Add a channel
 export const addChannel = ({ commit }, { channel }) => {
-  return axios.post('api', {"method_name": "retrieve_feed", "method_args": [channel.url]}).then(res => {
+  return axios.post('/?method_name=retrieve_feed&method_args=' + channel.url + '&user=0').then(res => {
     let newChannel = res.data;
 
     // append other info
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // change this url to your api
-  baseURL: 'http://localhost:5000/',
+  baseURL: 'http://ec2-54-165-106-231.compute-1.amazonaws.com:5000/',
   headers: {
     'X-Requested-With': 'XMLHttpRequest'
   }
